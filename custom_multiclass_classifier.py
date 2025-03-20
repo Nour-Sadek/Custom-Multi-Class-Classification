76b931b77fa96f0480ed074cd554e20404688ecf
Nour Sadek <nour.sadek@mail.utoronto.ca>
2025-03-20 19:48:57 -0400

    Finished writing the softmax regression algorithm

--- custom_multiclass_classifier.py
+++ custom_multiclass_classifier.py
@@ -14,6 +14,8 @@ class CustomMultiClassClassifier:
     _loss: list[float]
 
     def __init__(self, learning_rate: float, iterations: int) -> None:
+        """Initialize a new CustomMultiClassClassifier instance with _learning_rate <learning_rate> and _iterations
+        <iterations>."""
         if learning_rate <= 0:
             raise ValueError("learning rate should be greater than 0")
         if int(iterations) != iterations or iterations <= 0:
@@ -53,6 +55,7 @@ class CustomMultiClassClassifier:
         pass
 
     def accuracy(self, y: np.ndarray, y_predictions: np.ndarray) -> float:
+        """Return the accuracy of the prediction <y_predictions> in comparison to the actual classification <y>."""
         comparison = (y == y_predictions)
         return float(comparison.sum() / y.size)
 
@@ -61,11 +64,11 @@ class SoftmaxRegression(CustomMultiClassClassifier):
 
     """Using the Softmax algorithm to perform multi-class Logistic Regression Classification"""
 
-    def softmax(self, X_by_class: np.ndarray) -> np.ndarray:
+    def softmax(self, X_per_class: np.ndarray) -> np.ndarray:
         """Return the output of applying the softmax function on the <X_by_class> array of shape
         nb_observations x nb_classes where the weights assigned to each have already been applied to their
         respective features."""
-        X_per_class_stable = X_by_class - np.max(X_by_class)
+        X_per_class_stable = X_per_class - np.max(X_per_class)
         probs_per_class = np.exp(X_per_class_stable) / np.sum(np.exp(X_per_class_stable))
         return probs_per_class
 
@@ -76,20 +79,55 @@ class SoftmaxRegression(CustomMultiClassClassifier):
         return - (1 / design_matrix.shape[0]) * np.sum(design_matrix * np.log(probs_per_class + 1e-120))
 
     def fit(self, X_train: np.ndarray, y_train: np.ndarray) -> None:
-        return None
+        """Update the <self._weights> and <self._bias> terms trained on the training set <X_train> of shape
+        n_observations x n_features and the target variables for each observation <y_train> using Cross Entropy as the
+        cost function and Gradient Descent as the optimization algorithm."""
+        # Set up initial values of <self._weights> and <self._bias>
+        nb_observations, nb_features = X_train.shape
+        nb_classes = np.unique(y_train).size
+        self._weights = np.zeros((nb_features, nb_classes))
+        self._bias = np.zeros((1, nb_classes))
 
-    def predict(self, X: np.ndarray) -> np.ndarray:
-        pass
+        # Create the design matrix for <X_train>
+        design_matrix = np.eye(nb_classes)[y_train]
+
+        # If training the model again, remove saved information from previous run
+        self._accuracy = []
+        self._loss = []
+
+        for _ in range(self._iterations):
+            # Apply the softmax regression function
+            X_per_class = (X_train @ self._weights) + self._bias
+            probs_per_class = self.softmax(X_per_class)
+
+            # Update the weights and bias terms for each class
+            d_weights = (1 / nb_observations) * (X_train.T @ (probs_per_class - design_matrix))
+            self._weights = self._weights - self._learning_rate * d_weights
+            d_bias = (1 / nb_observations) * (np.sum(probs_per_class - design_matrix, axis=0, keepdims=True))
+            self._bias = self._bias - self._learning_rate * d_bias
+
+            # Compute the loss and accuracy
+            loss = self.cross_entropy_loss(design_matrix, probs_per_class)
+            accuracy = self.accuracy(y_train, np.argmax(probs_per_class, axis=1))
+            self._accuracy.append(accuracy)
+            self._loss.append(loss)
+
+    def predict(self, X_test: np.ndarray) -> np.ndarray:
+        """Return a 1D numpy array assigning predicted classifications based on classes seen during training for the
+        observations in <X_test> of shape nb_observations x nb_features"""
+        X_per_class = (X_test @ self._weights) + self._bias
+        probs_per_class = self.softmax(X_per_class)
+        return np.argmax(probs_per_class, axis=1)
 
 
 class OneVsRestRegression(CustomMultiClassClassifier):
 
-    def fit(self):
+    def fit(self, X_train: np.ndarray, y_train: np.ndarray) -> None:
         return
 
 
 class OneVsOneRegression(CustomMultiClassClassifier):
 
-    def fit(self):
+    def fit(self, X_train: np.ndarray, y_train: np.ndarray) -> None:
         return
 
