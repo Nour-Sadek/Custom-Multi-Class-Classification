f597a87999a84817812e5bc174c142d181970c97
Nour Sadek <nour.sadek@mail.utoronto.ca>
2025-03-20 20:50:17 -0400

    Fixed a couple of bugs and errors and added plotting function

--- custom_multiclass_classifier.py
+++ custom_multiclass_classifier.py
@@ -45,8 +45,12 @@ class CustomMultiClassClassifier:
         return self._bias
 
     @property
-    def history(self) -> dict[str: list]:
-        return self._history
+    def accuracy(self) -> list[float]:
+        return self._accuracy
+
+    @property
+    def loss(self) -> list[float]:
+        return self._loss
 
     def fit(self, X_train: np.ndarray, y_train: np.ndarray) -> None:
         pass
@@ -54,7 +58,7 @@ class CustomMultiClassClassifier:
     def predict(self, X:np.ndarray) -> np.ndarray:
         pass
 
-    def accuracy(self, y: np.ndarray, y_predictions: np.ndarray) -> float:
+    def determine_accuracy(self, y: np.ndarray, y_predictions: np.ndarray) -> float:
         """Return the accuracy of the prediction <y_predictions> in comparison to the actual classification <y>."""
         comparison = (y == y_predictions)
         return float(comparison.sum() / y.size)
@@ -64,19 +68,22 @@ class SoftmaxRegression(CustomMultiClassClassifier):
 
     """Using the Softmax algorithm to perform multi-class Logistic Regression Classification"""
 
+    def __init__(self, learning_rate: float, iterations: int) -> None:
+        super().__init__(learning_rate, iterations)
+
     def softmax(self, X_per_class: np.ndarray) -> np.ndarray:
         """Return the output of applying the softmax function on the <X_by_class> array of shape
         nb_observations x nb_classes where the weights assigned to each have already been applied to their
         respective features."""
-        X_per_class_stable = X_per_class - np.max(X_per_class)
-        probs_per_class = np.exp(X_per_class_stable) / np.sum(np.exp(X_per_class_stable))
+        X_per_class_stable = X_per_class - np.max(X_per_class, axis=1, keepdims=True)
+        probs_per_class = np.exp(X_per_class_stable) / np.sum(np.exp(X_per_class_stable), axis=1, keepdims=True)
         return probs_per_class
 
     def cross_entropy_loss(self, design_matrix: np.ndarray, probs_per_class: np.ndarray) -> float:
         """Return the cross entropy loss value between the expected <design_matrix> probabilities and the predicted
         <probs_per_class> probabilities of class affiliation where both arrays are of shape n_observations x n_classes"""
         # 1e-20 is added to prevent the possibility of encountering a np.log(0) value
-        return - (1 / design_matrix.shape[0]) * np.sum(design_matrix * np.log(probs_per_class + 1e-120))
+        return float(- (1 / design_matrix.shape[0]) * np.sum(design_matrix * np.log(probs_per_class + 1e-120)))
 
     def fit(self, X_train: np.ndarray, y_train: np.ndarray) -> None:
         """Update the <self._weights> and <self._bias> terms trained on the training set <X_train> of shape
@@ -108,7 +115,7 @@ class SoftmaxRegression(CustomMultiClassClassifier):
 
             # Compute the loss and accuracy
             loss = self.cross_entropy_loss(design_matrix, probs_per_class)
-            accuracy = self.accuracy(y_train, np.argmax(probs_per_class, axis=1))
+            accuracy = self.determine_accuracy(y_train, np.argmax(probs_per_class, axis=1))
             self._accuracy.append(accuracy)
             self._loss.append(loss)
 
@@ -118,6 +125,14 @@ class SoftmaxRegression(CustomMultiClassClassifier):
         X_per_class = (X_test @ self._weights) + self._bias
         probs_per_class = self.softmax(X_per_class)
         return np.argmax(probs_per_class, axis=1)
+
+    def plot_loss(self) -> None:
+        plt.plot(self._loss)
+        plt.title("Model Cross Entropy Loss")
+        plt.ylabel("Cross Entropy loss")
+        plt.xlabel("Iteration")
+        plt.legend(["train"], loc="upper left")
+        plt.show()
 
 
 class OneVsRestRegression(CustomMultiClassClassifier):
